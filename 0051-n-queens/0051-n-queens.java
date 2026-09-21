a66cece042794c45ccc633902ea3acc5fe19ca43
anshikamonga001 <156787119+anshikamonga001@users.noreply.github.com>
2026-09-21 18:18:21 +0530

    Time: 2 ms (81.98%), Space: 46.5 MB (81%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+class Solution {
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+
+        char[][] board = new char[n][n];
+
+        // Fill board with '.'
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        solve(0, n, board, ans);
+
+        return ans;
+    }
+
+    public void solve(int row, int n, char[][] board,
+                      List<List<String>> ans) {
+
+        // All queens placed
+        if (row == n) {
+            List<String> temp = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                temp.add(new String(board[i]));
+            }
+
+            ans.add(temp);
+            return;
+        }
+
+        // Try placing queen in every column
+        for (int col = 0; col < n; col++) {
+
+            if (isSafe(row, col, n, board)) {
+
+                board[row][col] = 'Q';
+
+                solve(row + 1, n, board, ans);
+
+                // Backtrack
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    public boolean isSafe(int row, int col, int n, char[][] board) {
+
+        // Check same column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1;
+             i >= 0 && j >= 0;
+             i--, j--) {
+
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1;
+             i >= 0 && j < n;
+             i--, j++) {
+
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
